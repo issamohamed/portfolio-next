c0cca5ad78bfd1c7d16bbdc264d7d7d22dc959ec
Issa Mohamed <48192737+issamohamed@users.noreply.github.com>
2025-03-24 15:27:51 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/portfolio-next',
-    images: {
-      unoptimized: true,
-    },
-  }
   
-  module.exports = nextConfig
+  images: {
+    domains: ['*'], 
+  },
+}
+
+module.exports = nextConfig
